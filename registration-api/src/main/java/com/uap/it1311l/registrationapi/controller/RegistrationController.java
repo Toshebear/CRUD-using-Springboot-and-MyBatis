592c4a81cdf34e20d7981d0cb063ecf6dc608389
Jethro Jumawan <jethroluis.jumawan@stratpoint.com>
2023-10-26 23:12:31 +0800

    Final push

---
+++ registration-api/src/main/java/com/uap/it1311l/registrationapi/controller/RegistrationController.java
@@ -0,0 +1,44 @@
+package com.uap.it1311l.registrationapi.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.uap.it1311l.registrationapi.model.Attendee;
+import com.uap.it1311l.registrationapi.repository.RegistrationMybatisRepository;
+
+@RestController
+public class RegistrationController {
+	@Autowired
+	RegistrationMybatisRepository registrationRepo;
+	
+	@PostMapping("/event/register")
+	public Attendee register(@RequestBody Attendee student) {
+		registrationRepo.insert(student);
+		return student; 
+	}
+	
+	@GetMapping("/attendees")
+	public Attendee[] attendees() {
+		return registrationRepo.findAll(); 
+	}
+	
+	@PutMapping("/event/survey/eligibility")
+	public List<Attendee> updateEligibility() {
+		registrationRepo.updateEligibility();
+		List<Attendee> attendees = registrationRepo.getAllEligible();
+		return attendees;
+	}
+	
+	@DeleteMapping("/event/registration/{id}")
+	public void deleteAttendee(@PathVariable("id") int attendeeId) {
+		registrationRepo.deleteId(attendeeId);
+	}
+}
---
+++ registration-api/src/main/java/com/uap/it1311l/registrationapi/repository/RegistrationMybatisRepository.java
@@ -0,0 +1,31 @@
+package com.uap.it1311l.registrationapi.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import com.uap.it1311l.registrationapi.model.Attendee;
+
+@Mapper
+public interface RegistrationMybatisRepository {
+	@Insert
+	("INSERT INTO attendees(firstName, middleName, lastName, course, age) VALUES (#{firstName}, #{middleName}, #{lastName}, #{course}, #{age})")
+	public int insert(Attendee attendee);
+	
+	@Select
+	("SELECT * FROM attendees")
+    public Attendee[] findAll();
+	
+	@Update("UPDATE attendees SET surveyEligibilityTag = true WHERE course = 'BSIT'")
+	public int updateEligibility();
+	
+	@Select("SELECT * FROM attendees WHERE surveyEligibilityTag = true")
+	public List<Attendee> getAllEligible();
+	
+	@Delete("DELETE FROM attendees WHERE attendeeId = #{attendeeId}")
+	public int deleteId(int attendeeId);
+}
